caf6b5b5dd2c278afeb10f80cdb73a62a2e5b62a
Rinalds Viksna <rinaldsviksna@gmail.com>
2017-08-17 23:55:41 +0300

    stila uzlabojumi

--- views/tweet/evaluate.php
+++ views/tweet/evaluate.php
@@ -9,19 +9,19 @@ use app\models\Sentiment;
 
 $this->title = $model->id;
 ?>
-<div class="tweet-eval">
+<div class="tweet-eval center">
 
-    <h1><?= "Novērtē tvītu  " . Html::encode($this->title) ?></h1>
+    <h1><?= "Novērtē tvītu  " //. Html::encode($this->title) ?></h1>
 	
-	<div class="row"><?= $message ?></div>
+	
     <div class="row">
    		<?= Html::a(Html::img(
                 $model->author_pic,
                 ['alt'=>$model->author_name]), 
-                $model->author_profile, ['class'=>"img-author"]
+   		        $model->author_profile, ['class'=>"img-author",'target'=>'_blank']
             )?>
    		<div class="tweet-text">
-   		<?= $model->text ?>
+   		<?= Html::a($model->text,"https://twitter.com/statuses/".Html::encode($this->title),['target'=>'_blank']) ?>
 		</div>
 	</div>
 	<div class="evaluation-buttons row">
@@ -34,9 +34,6 @@ $this->title = $model->id;
         echo Html::a("Tvīts nav latviešu valodā",['rate','id'=>$model->id,'latvian'=>false],['class' => 'btn btn-danger']);
     ?>
     </div>
-
-    
-
-
+    <div class="row"><?= $message ?></div>
 
 </div>
